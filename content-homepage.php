bcda9882068140c1b14ac86ea17efc48fea66e4b
dpavlovic <vatrom@gmail.com>
2013-08-12 12:35:45 +0200

    home-page query

--- content-homepage.php
+++ content-homepage.php
@@ -1,7 +1,36 @@
 <div class="content">
 
 	<ul>
-		<li>
+		<?php 
+			$queryHomePageArgs = array (
+					'category_name' => 'obavijesti-i-novosti',
+				)
+			$queryHomePage = new WP_Query($queryHomePageArgs);
+			if($queryHomePage->have_posts()){
+				while($queryHomePage->have_posts()){
+					$queryHomePage->the_post();?>
+					<li>
+						<h2><a href="">Dan vode 22. ožujka</a></h2>
+						<div class="info clearfix">
+							<div><span>Author:</span> Admin</div>
+							<div><span>Objavljeno:</span> 16.10.2011.</div>
+							<div><span>Kategorija:</span> <a href="">Obavijesti</a></div>
+						</div>
+						<div class="info-text">
+							<p>
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+							</p>
+						</div>
+					</li>
+
+				<?php }
+
+			} else {
+				echo "Nema unosa koji zadvoljava uvjet!";
+			}
+			wp_reset_postdata();
+		?>
+		<!--<li>
 			<h2><a href="">Dan vode 22. ožujka</a></h2>
 			<div class="info clearfix">
 				<div><span>Author:</span> Admin</div>
@@ -52,7 +81,7 @@
 					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 				</p>
 			</div>
-		</li>
+		</li>-->
 	</ul>
 
 	<div class="navigation">
