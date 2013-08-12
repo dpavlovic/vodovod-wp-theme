802f2b0a278a09b427c4996b5265be97f225c8a6
dpavlovic <vatrom@gmail.com>
2013-08-12 15:33:04 +0200

    category layout

--- category.php
+++ category.php
@@ -1,38 +1,45 @@
-<div class="content">
-	<div class="info-text">
-		<?php category_description()?>
+<?php get_header( $name = null ); ?>
+<!-- start:main -->
+<section class="main">
+
+	<!-- start:container -->
+	<div class="container clearfix">
+
+		<!-- start:content -->
+		<?php get_template_part( 'content', $name = 'category' )?>
+		<!-- stop:content -->
+
+		<aside>
+
+			<div class="box">
+				<h3>Uplate putem interneta:</h3>
+				<a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/komis.gif" alt="" /></a>
+			</div>
+
+			<div class="box">
+				<h3>Koirisni linkovi:</h3>
+				<ul>
+					<li><a href="">www.undergroundwater.org</a></li>
+					<li><a href="">www.azto.hr</a></li>
+					<li><a href="">www.gvik.hr</a></li>
+				</ul>
+			</div>
+
+			<div class="box">
+				<h3>kategorije:</h3>
+				<ul>
+					<li><a href="">Javna nabava</a></li>
+					<li><a href="">Obavijesti</a></li>
+					<li><a href="">Servisne informacije</a></li>
+					<li><a href="">Zakoni, pravilnici, odluke</a></li>
+				</ul>
+			</div>
+
+		</aside>
+
 	</div>
-	<ul>
-		<?php 
-			if(have_posts()){
-				while(have_posts()){
-					the_post();?>
-					<li>
-						<h2><a href="<?php the_permalink();?>"><?php the_title( $before = '', $after = '', $echo = true )?></a></h2>
-						<div class="info clearfix">
-							<div><span>Author:</span><?php the_author( $deprecated = '', $deprecated_echo = true )?></div>
-							<div><span>Objavljeno:</span><?php the_date( $d = '', $before = '', $after = '', $echo = true )?></div>
-							<div><span>Kategorija:</span> <?php the_category( $separator = ',', $parents='', $post_id = false )?></div>
-						</div>
-						<div class="info-text">
-							<?php the_content( $more_link_text = null, $stripteaser = false );?>
-						</div>
-					</li>
+	<!-- stop:container -->
 
-				<?php }
-
-			} else {
-				echo "Nema unosa koji zadvoljava uvjet!";
-			}
-			wp_reset_postdata();
-		?>
-		</ul>
-
-	<div class="navigation">
-		<p>
-			<a href="http://www.designtheplanet.com/news/">« Stariji textovi</a> — 
-			<a href="http://www.designtheplanet.com/news/page/3/">Noviji tekstovi »</a>
-		</p>
-	</div>
-
-</div>
+</section>
+<!-- stop:main -->
+<?php get_footer( $name = null );?>
