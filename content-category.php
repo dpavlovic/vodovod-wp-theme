d69b449c1b9ed73ad3cc86345bb98aad16d3baaf
dpavlovic <vatrom@gmail.com>
2013-08-13 20:34:07 +0200

    condition showing of post publish time

--- content-category.php
+++ content-category.php
@@ -11,7 +11,7 @@
 						<h2><a href="<?php the_permalink();?>"><?php the_title( $before = '', $after = '', $echo = true )?></a></h2>
 						<div class="info clearfix">
 							<div><span>Author:</span><?php the_author( $deprecated = '', $deprecated_echo = true )?></div>
-							<div><span>Objavljeno:</span><?php the_time( $d = 'F j, Y' )?></div>
+							<?php if(is_category( $category = 'obavijesti-i-novosti' )) { ?><div><span>Objavljeno:</span><?php the_time( $d = 'F j, Y' )?></div><?php } ?>
 							<div><span>Kategorija:</span> <?php the_category( $separator = ',', $parents='', $post_id = false )?></div>
 						</div>
 						<div class="info-text">
