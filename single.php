079df5992cede48b9873a750bd14117740bdf758
dpavlovic <vatrom@gmail.com>
2013-08-25 21:07:01 +0200

    adding sidebar layout on frontpage and category view

--- single.php
+++ single.php
@@ -9,34 +9,6 @@
 		<?php get_template_part( 'content', $name = 'single' )?>
 		<!-- stop:content -->
 		<?php get_sidebar( $name = "right" ) ?>
-		<!--<aside>
-
-			<div class="box">
-				<h3>Uplate putem interneta:</h3>
-				<a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/komis.gif" alt="" /></a>
-			</div>
-
-			<div class="box">
-				<h3>Koirisni linkovi:</h3>
-				<ul>
-					<li><a href="">www.undergroundwater.org</a></li>
-					<li><a href="">www.azto.hr</a></li>
-					<li><a href="">www.gvik.hr</a></li>
-				</ul>
-			</div>
-
-			<div class="box">
-				<h3>kategorije:</h3>
-				<ul>
-					<li><a href="">Javna nabava</a></li>
-					<li><a href="">Obavijesti</a></li>
-					<li><a href="">Servisne informacije</a></li>
-					<li><a href="">Zakoni, pravilnici, odluke</a></li>
-				</ul>
-			</div>
-
-		</aside>-->
-
 	</div>
 	<!-- stop:container -->
 
