97a3515814d0de7010288bb35b365373578906c4
dpavlovic <vatrom@gmail.com>
2013-08-26 07:53:57 +0200

    remove unneeded parts from sidebar

--- sidebar-right.php
+++ sidebar-right.php
@@ -1,27 +1,3 @@
 <aside>
 	<?php dynamic_sidebar( 'rightsidebar' ) ?>
-	<div class="box">
-		<h3>Uplate putem interneta:</h3>
-		<a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/komis.gif" alt="" /></a>
-	</div>
-
-	<div class="box">
-		<h3>Koirisni linkovi:</h3>
-		<ul>
-			<li><a href="">www.undergroundwater.org</a></li>
-			<li><a href="">www.azto.hr</a></li>
-			<li><a href="">www.gvik.hr</a></li>
-		</ul>
-	</div>
-
-	<div class="box">
-		<h3>kategorije:</h3>
-		<ul>
-			<li><a href="">Javna nabava</a></li>
-			<li><a href="">Obavijesti</a></li>
-			<li><a href="">Servisne informacije</a></li>
-			<li><a href="">Zakoni, pravilnici, odluke</a></li>
-		</ul>
-	</div>
-
 </aside>
