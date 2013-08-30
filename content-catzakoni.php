f023d2d92643adb6eb85690835c76080a0ecd2c4
dpavlovic <vatrom@gmail.com>
2013-08-30 12:50:05 +0200

    metabox for specific category and layout

---
+++ content-catzakoni.php
@@ -0,0 +1,33 @@
+<div class="content">
+	<div class="info-text">
+		<?php echo category_description()?>
+	</div>
+	<ul>
+		<?php 
+		if(have_posts()){
+			while(have_posts()){
+				the_post();?>
+				<li>
+					<a href="<?php the_permalink();?>"><div class="post-thumbnail"><img src="<?php echo get_template_directory_uri();?>/images/document-icon.png"></div></a>
+					<div class="gallery-info"><h2><a href="<?php the_permalink();?>"><?php the_title( $before = '', $after = '', $echo = true )?></a></h2>
+						<div class="info clearfix">
+							<?php if(is_category( $category = 'obavijesti-i-novosti' )) { ?><div><span>Objavljeno:</span><?php the_time( $d = 'F j, Y' )?></div><?php } ?></div>
+
+							<div class="clearfix"><span>Kategorija:</span> <?php the_category( $separator = ',', $parents='', $post_id = false )?></div>
+							<div class="link clearfix"><a href="<?php echo get_post_meta($post->ID, 'dbt_url', true); ?>" >Preuzimanje dokumenta</a></div>
+							<div class="description clearfix">Opis: <?php echo get_post_meta($post->ID, 'dbt_opis', true); ?></div>
+						</div>
+						<div class="info-text">
+							<?php //the_content( $more_link_text = "Pročitaj više ...", $stripteaser = false );?>
+						</div>
+					</li>
+
+					<?php }
+
+				} else {
+					echo "Nema unosa koji zadvoljava uvjet!";
+				}
+				?>
+			</ul>
+
+		</div>
