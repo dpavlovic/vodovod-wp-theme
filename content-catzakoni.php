ddc3a539575b5cf9f9e42e688c53714f75d2d55b
dpavlovic <vatrom@gmail.com>
2013-09-03 19:10:50 +0200

    more category law

--- content-catzakoni.php
+++ content-catzakoni.php
@@ -15,7 +15,7 @@
 
 							<div class="clearfix"><span>Kategorija:</span> <?php the_category( $separator = ',', $parents='', $post_id = false )?></div>
 							<div class="link clearfix"><a href="<?php echo get_post_meta($post->ID, 'dbt_url', true); ?>" >Preuzimanje dokumenta</a></div>
-							<div class="description clearfix"><span>Opis:<span> <?php echo get_post_meta($post->ID, 'dbt_opis', true); ?></div>
+							<div class="description clearfix"><span>Opis:</span> <?php echo get_post_meta($post->ID, 'dbt_opis', true); ?></div>
 						</div>
 						<div class="info-text">
 							<?php //the_content( $more_link_text = "Pročitaj više ...", $stripteaser = false );?>
